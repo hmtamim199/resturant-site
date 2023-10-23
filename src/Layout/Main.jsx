ccdde7bb5c23706573f91dba9a39964f60b0d1c3
hmtamim199 <hmtamim8906@gmail.com>
2023-10-23 11:50:15 +0600

    slider added

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,12 +1,12 @@
 
 import { Outlet } from 'react-router-dom';
-import Header from '../Pages/Shared/Header/Header';
 import Footer from '../Pages/Shared/Footer/Footer';
+import Navbar from '../Pages/Shared/Navbar/Navbar';
 
 const Main = () => {
   return (
     <div>
-      <Header></Header>
+      <Navbar></Navbar>
       <Outlet></Outlet>
       <Footer></Footer>
     </div>
---
+++ src/Pages/Home/Category/Category.jsx
@@ -0,0 +1,44 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import slider1 from '../../../assets/home/slide1.jpg';
+import slider2 from '../../../assets/home/slide2.jpg';
+import slider3 from '../../../assets/home/slide3.jpg';
+import slider4 from '../../../assets/home/slide4.jpg';
+import slider5 from '../../../assets/home/slide5.jpg';
+
+
+const Category = () => {
+  return (
+    <Swiper
+      slidesPerView={4}
+      spaceBetween={30}
+      centeredSlides={true}
+      pagination={{
+        clickable: true,
+      }}
+      modules={[Pagination]}
+      className="mySwiper"
+    >
+      <SwiperSlide>
+        <img src={slider1} />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img src={slider2} />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img src={slider3} />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img src={slider4} />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img src={slider5} />
+      </SwiperSlide>
+
+    </Swiper>
+  );
+};
+
+export default Category;
