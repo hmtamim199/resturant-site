874eb1cf7280e347b7328156620ecf3329dbb201
hmtamim199 <hmtamim8906@gmail.com>
2023-10-26 00:43:45 +0600

    menu bar added

---
+++ src/Pages/Home/PopularMenu/PopularMenu.jsx
@@ -0,0 +1,41 @@
+import { useEffect, useState } from "react";
+import SectionTitle from "../../../Components/SectionTitle/SectionTitle";
+import MenuItem from "../../Shared/MenuItem/MenuItem";
+
+const PopularMenu = () => {
+
+  const [menu, setMenu] = useState([])
+  useEffect(() => {
+    fetch('menu.json')
+      .then(res => res.json())
+      .then(data => {
+
+        const popularItems = data.filter(item => item.category === 'popular');
+        setMenu(popularItems)
+      })
+  }, [])
+  return (
+    <section className="mb-6 mt-4">
+
+      <SectionTitle
+        subHeadingTitle={' popular menu'}
+        headingTitle={'FROM OUR MENU'}
+
+      ></SectionTitle>
+
+      <div className="grid md:grid-cols-2 gap-10">
+        {
+          menu.map(item => <MenuItem
+            key={item._id}
+            item={item}
+          ></MenuItem>)
+        }
+      </div>
+
+    </section>
+
+
+  );
+};
+
+export default PopularMenu;
