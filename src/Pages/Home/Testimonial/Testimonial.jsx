0b66e6470719c7568a470b6699a3a5253267b698
hmtamim199 <hmtamim8906@gmail.com>
2023-10-27 19:14:01 +0600

    reviews added

--- src/Pages/Home/Testimonial/Testimonial.jsx
+++ src/Pages/Home/Testimonial/Testimonial.jsx
@@ -1,10 +1,43 @@
+import SectionTitle from "../../../Components/SectionTitle/SectionTitle";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css/navigation';
+
+import { Navigation } from 'swiper/modules';
+import { useEffect, useState } from "react";
 
 
 const Testimonial = () => {
-  return (
-    <div>
 
-    </div>
+  const [reviews, setReviews] = useState([]);
+  useEffect(() => {
+    fetch('testimonial.json')
+      .then(res => res.json())
+      .then(data => setReviews(data))
+  }, [])
+
+  return (
+    <section>
+      <SectionTitle
+
+        headingTitle={"what our client says"}
+        subHeadingTitle={"testimnila"}
+      > </SectionTitle>
+      <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
+
+        {
+          reviews.map(review => <SwiperSlide
+            key={review._id}
+
+          >
+            <div>
+              <p>{review.details}</p>
+              <h className="text-2xl text-orange-500">{review.name}</h>
+            </div>
+          </SwiperSlide>)
+        }
+
+      </Swiper>
+    </section>
   );
 };
 
