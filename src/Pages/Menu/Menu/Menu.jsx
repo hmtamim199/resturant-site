4c8d436e57cdcc7c51be505124e592d5c8b0d4c3
hmtamim199 <hmtamim8906@gmail.com>
2023-11-13 19:16:24 +0600

    parallax added

--- src/Pages/Menu/Menu/Menu.jsx
+++ src/Pages/Menu/Menu/Menu.jsx
@@ -1,4 +1,7 @@
 import { Helmet } from 'react-helmet-async';
+import Cover from '../../Shared/Cover/Cover';
+import menu from '../../../assets/menu/dessert-bg.jpeg'
+import PopularMenu from '../../Home/PopularMenu/PopularMenu';
 
 
 const Menu = () => {
@@ -8,6 +11,12 @@ const Menu = () => {
         <title>Bistro Boss | Menu</title>
 
       </Helmet>
+      <Cover img={menu} title="our menu"></Cover>
+      <PopularMenu></PopularMenu>
+      <Cover img={menu} title="our menu"></Cover>
+      <PopularMenu></PopularMenu>
+      <Cover img={menu} title="our menu"></Cover>
+      <PopularMenu></PopularMenu>
     </div>
   );
 };
